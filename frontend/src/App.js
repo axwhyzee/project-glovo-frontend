80033e7cc0b72608e66efb8fe1b36f3f5b7956c7
Yuandjom <ziyangjohn@gmail.com>
2023-02-20 00:24:52 +0800

    testing about page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route, BrowserRouter } from "react-router-dom";
+import Home from "./pages/home/Home";
+import About from "./pages/about/About";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/about" element={<About />}></Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ frontend/src/pages/about/About.jsx
@@ -0,0 +1,68 @@
+import React from 'react'
+import "./about.scss"
+
+function About() {
+  return (
+    <>
+
+    <header>
+        <div class="title">
+            <h3>The creative crew</h3>
+        </div>
+        <div class="content">
+            <h5>who we are</h5>
+            <p>We are team of creatively diverse. driven. innovative individuals working in various locations from the world.</p>
+        </div>
+    </header>
+
+    <main>
+        <div class="profile">
+                <figure data-value="product owner">
+                    <img src="https://rvs-team-page.onrender.com/photo1.png" alt=""/>
+                    <figcaption>bill mahoney</figcaption>
+                </figure>
+            </div>
+
+            <div class="profile">
+                <figure data-value="art director">
+                    <img src="https://rvs-team-page.onrender.com/photo2.png" alt=""/>
+                    <figcaption>saba cabrera</figcaption>
+                </figure>
+            </div>
+
+            <div class="profile">
+                <figure data-value="tech lead">
+                    <img src="https://rvs-team-page.onrender.com/photo3.png" alt=""/>
+                    <figcaption>shae le</figcaption>
+                </figure>
+            </div>
+
+            <div class="profile">
+                <figure data-value="ux designer">
+                    <img src="https://rvs-team-page.onrender.com/photo4.png" alt=""/>
+                    <figcaption>skylah lu</figcaption>
+                </figure>
+            </div>
+
+            <div class="profile">
+                <figure data-value="developer">
+                    <img src="https://rvs-team-page.onrender.com/photo5.png" alt=""/>
+                    <figcaption>griff richards</figcaption>
+                </figure>
+            </div>
+
+            <div class="profile">
+                <figure data-value="developer">
+                    <img src="https://rvs-team-page.onrender.com/photo6.png" alt=""/>
+                    <figcaption>stan john</figcaption>
+                </figure>
+            </div>
+            </main>
+
+
+
+    </>
+  )
+}
+
+export default About
