c239e37e9800726858598899c042e1b4f8611222
Yuandjom <ziyangjohn@gmail.com>
2023-03-11 23:30:54 +0800

    sidebar search not working -.-

--- frontend/src/pages/home/Sidebar.jsx
+++ frontend/src/pages/home/Sidebar.jsx
@@ -4,11 +4,13 @@ import React, { useState, useEffect } from 'react'
 import './home.scss';
 import Box from '@mui/material/Box';
 import IndivPost from '../../components/IndivPost';
-
+import {Search} from  "@mui/icons-material"
+import {Typography,IconButton, InputBase} from '@mui/material'
 
 function Sidebar() {
     const [toggle, SetToggle] = useState(true);
     const [posts, setPosts] = useState(null)
+    const [searchInput, setSearchInput] = useState("");
 
     // we only need to render this once, so the dependency array is empty
     useEffect(() => {
@@ -28,7 +30,17 @@ function Sidebar() {
         const data = await response.json();
         setPosts(data);
       };
-
+    const handleChange = (e) => {
+        e.preventDefault();
+        setSearchInput(e.target.value);
+            if (searchInput.length > 0) {
+                posts.filter((post) => {
+                return post.publisher.match(searchInput);
+            });
+        };
+    }
+    
+    //mount the thing
     if(!posts){
         return null
     }
@@ -38,6 +50,8 @@ function Sidebar() {
             <button className='toggle-sidebar' type='button' onClick={() => { SetToggle(!toggle) }}>
                 <FontAwesomeIcon icon={toggle ? faCaretRight : faCaretLeft} />
             </button>
+
+
             <Box 
                 display="flex" 
                 flexDirection={'column'} 
@@ -46,6 +60,28 @@ function Sidebar() {
                 alignItems={"center"}
                 gap = {2}
                 > 
+                <Box
+                    borderRadius="9px"
+                    gap="3rem"
+                    padding="0.1rem 1.5rem"
+                >
+                    <InputBase placeholder="Search..."
+                        sx={{
+                        backgroundColor: "white",
+                        borderRadius: "4rem",
+                        padding: "auto",
+                        margin: "auto"
+                        }}
+                        type="text"
+                        onChange={handleChange}
+                        value={searchInput}
+                    />
+                    <IconButton >
+                        <Search />
+                    </IconButton>
+                </Box>
+
+            
                 {posts && 
                     posts.map((item, index) => (
                     <IndivPost 
