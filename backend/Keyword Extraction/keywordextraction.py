b24237775d675495e6e8dc393a4c8dfbbe8d45cb
Chinmay-002 <90840442+Chinmay-002@users.noreply.github.com>
2023-03-21 21:38:27 +0800

    Add files via upload

--- backend/Keyword Extraction/keywordextraction.py
+++ backend/Keyword Extraction/keywordextraction.py
@@ -1,7 +1,7 @@
-from keybert import KeyBERT
 import json
-from sentence_transformers import SentenceTransformer
 import numpy as np
+from keybert import KeyBERT
+from keyphrase_vectorizers import KeyphraseCountVectorizer
 
 
 def getArticles():
@@ -14,18 +14,21 @@ def writeFile(data: str):
         file.write(data)
 
 
-def extractKeywords(data: str) -> list:
+def extractKeywords(data: str, heading: str) -> list:
     model = KeyBERT()
+    vectorizer = KeyphraseCountVectorizer()
+    heading_words = [heading_keyword[0] for heading_keyword in
+                     model.extract_keywords(heading, min_df=1, stop_words="english", vectorizer=vectorizer)]
     try:
-        doc_embeddings, word_embeddings = model.extract_embeddings(data, min_df=1, stop_words="english",
-                                                                   keyphrase_ngram_range=(1, 2))
-        keywords = model.extract_keywords(data, min_df=1, stop_words="english",
-                                          doc_embeddings=doc_embeddings,
-                                          word_embeddings=word_embeddings, keyphrase_ngram_range=(1, 2), use_mmr=True,
-                                          diversity=0.7, top_n=10, nr_candidates=20)
+        doc_embeddings, word_embeddings = model.extract_embeddings(data, stop_words="english")
+
+        keywords = model.extract_keywords(data, stop_words="english", seed_keywords=heading_words, top_n=5,
+                                          nr_candidates=10, diversity=.8, vectorizer=vectorizer, use_maxsum=True)
+
     except ValueError:
         return None, None
-    return keywords, word_embeddings
+
+    return keywords, word_embeddings, doc_embeddings
 
 
 def getPhraseEmbeddings(phrase: str, content_words: list[str], phrase_embeddings) -> list:
@@ -40,36 +43,58 @@ def getPhraseEmbeddings(phrase: str, content_words: list[str], phrase_embeddings
     return avg_embedding
 
 
+def get_cosine_similarity(a, b):
+    numerator = np.dot(a, b.transpose())
+
+    a_norm = np.sqrt(np.sum(a ** 2))
+    b_norm = np.sqrt(np.sum(b ** 2))
+
+    denominator = a_norm * b_norm
+
+    cosine_similarity = numerator / denominator
+
+    # print(cosine_similarity)
+    return cosine_similarity[0][0]
+
+
 article_list = []
 # get the list of articles from the json file
 json_data = getArticles()
 
-for article in json_data:
+word_embeddings_list = []
+
+for article in json_data[:25]:
     # get the content of the article, without caps
     content = article['Content'].lower()
 
     # extract the keywords from the content
-    keyphrases, embeddings = extractKeywords(content)
+    keyphrases, embeddings, doc_e = extractKeywords(content, article['Title'].lower())
+
     if keyphrases is None:
         continue
 
     words = [keyword[0] for keyword in keyphrases]
-    word_embeddings_list = []
+
     # iterate through the list of keywords
     for i in range(len(words)):
         # get the current phrase
         word = words[i]
+
         # split the content by space character into a list of words
         article_words = content.split()
+
         # get the average embedding of the phrase
         phrase_embedding = getPhraseEmbeddings(word, article_words, embeddings)
+
         if phrase_embedding is None:
             continue
+
+        word_embeddings_list.append([word, phrase_embedding])
+
         # create a dictionary of the phrase, similarity and average embedding
         word_dict = {"phrase": word, "similarity": keyphrases[i][1], "vectors": phrase_embedding}
         # append the dictionary to the list of keywords
-        word_embeddings_list.append(word_dict)
-
+    #
     # add the list of keywords to the article's dictionary
     article = dict(article)
     article['keywords'] = word_embeddings_list
@@ -78,3 +103,11 @@ for article in json_data:
 # write the list of articles to a json file
 articles_str = json.dumps(article_list)
 writeFile(articles_str)
+
+for i in range(len(word_embeddings_list)):
+    for j in range(i + 1, len(word_embeddings_list)):
+        output = round(get_cosine_similarity(np.asarray(word_embeddings_list[i][1]),
+                                             np.asarray(word_embeddings_list[j][1])) * 100, 1)
+        if output > 60:
+            print("{} and {} are {}% similar".format(word_embeddings_list[i][0],
+                                                     word_embeddings_list[j][0], output))
