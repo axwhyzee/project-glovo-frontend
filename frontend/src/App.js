4e4d25e6a4d64ca22ceeedd04addf33b0b494788
axwhyzee <siah_weehung@yahoo.com>
2023-06-14 01:17:27 +0800

    cleanup unused dependencies

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,6 @@ import React, { useState } from 'react';
 import { Routes, Route, BrowserRouter } from "react-router-dom";
 import Home from "./pages/home/Home";
 import About from "./pages/about/About";
-import Test from "./pages/test/Test";
 import DataLoader from './components/DataLoader';
 
 function App() {
@@ -12,9 +11,8 @@ function App() {
     <div>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Home graphData={graphData} postData={postData} />}></Route>
+          <Route path="/" element={<Home graphData={graphData}/>}></Route>
           <Route path="/about" element={<About />}></Route>
-          <Route path="/test" element={<Test />}></Route>
         </Routes>
       </BrowserRouter>
       <DataLoader setGraphData={setGraphData} setPostData={setPostData} />
