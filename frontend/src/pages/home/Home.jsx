459208a77c208cb6a850af1b9b8fe6bf2f2542e3
axwhyzee <siah_weehung@yahoo.com>
2023-06-14 01:16:55 +0800

    component integration

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -1,24 +1,36 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Sidebar from "./Sidebar";
 import Graph from '../../components/Graph';
-import Zoom from '../../components/Zoom';
 import Navbar from '../../components/navbar/Navbar';
 import Panel, { dummy } from '../../components/graph/Panel';
 import Resolver from '../../components/graph/Resolver';
+import { getClusterByKey } from '../../network/api';
+import Spinner from '../../components/Spinner';
 
 function Home({ graphData, postData }) {
   const [queries, setQueries] = useState(dummy);
   const [resolved, setResolved] = useState([]);
   const [isSideBarOpen, setIsSideBarOpen] = useState(false);
+  const [posts, setPosts] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const simulationTime = 6;
+  // callback
+  const selectNode = async (val) => {
+    setLoading(true);
+    const temp = await getClusterByKey(val);
+    if (posts) setIsSideBarOpen(true);
+    setPosts(temp);
+    setLoading(false);
+  }
 
   return (
     <div className='home'>
       <Navbar />
-      <Sidebar data={postData} isSideBarOpen={isSideBarOpen} setIsSideBarOpen={setIsSideBarOpen} />
-      <Panel data={queries} update={setQueries} isSideBarOpen={isSideBarOpen}/>
-      <Zoom />
-      <Graph data={graphData} highlight={resolved} />
+      <Sidebar posts={posts} isSideBarOpen={isSideBarOpen} setIsSideBarOpen={setIsSideBarOpen} selectNode={selectNode}/>
+      <Panel data={queries} update={setQueries} isSideBarOpen={isSideBarOpen} simulationTime={simulationTime}/>
+      <Graph data={graphData} selectNode={selectNode} highlight={resolved} cola_engine={false} simulationTime={simulationTime} setLoading={setLoading}/>
       <Resolver data={graphData} queries={queries} update={setResolved}/>
+      {loading && <Spinner />}
     </div>
   )
 }
